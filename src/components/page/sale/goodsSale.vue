9364e29d44d45ec673832c9d50e0a2252d4e1fd3
Administrator <2329706697@qq.com>
2020-03-15 00:52:19 +0800

    修改

--- src/components/page/sale/goodsSale.vue
+++ src/components/page/sale/goodsSale.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <div>
+        <div class="mainDiv">
             <div class="proinfoLeft">
                 <div class="imgzoom-main">
                     <img v-if="goodsInfo.goodsId" :src="pictureUrl+goodsInfo.goodsId+'/'+imgShow">
@@ -13,9 +13,57 @@
                     </ul>
                 </div>
             </div>
-            <div class="proinfoMain">
 
+            <div class="proinfoMain">
+                <div class="proinfo-title">
+                    <h1>{{goodsInfo.goodsName}} {{goodsInfo.memo}}</h1>
+                </div>
+                <div class="goods-publish">
+                    <el-row>
+                        <el-col :span="6">
+                            <span class="i-header">型号: </span><span>{{goodsInfo.goodsType}}</span>
+                        </el-col>
+                        <el-col :span="6">
+                            <span class="i-header">颜色: </span><span>{{goodsInfo.specColor}}</span>
+                        </el-col>
+                        <el-col :span="6">
+                            <span class="i-header">材质: </span><span>{{goodsInfo.specMaterial}}</span>
+                        </el-col>
+                        <el-col :span="6">
+                            <span class="i-header">尺寸: </span><span>{{goodsInfo.specSize}}</span>
+                        </el-col>
+                    </el-row>
+                </div>
+                <div class="proinfo-focus">
+                    <el-row>
+                        <el-col :span="2">
+                            <span class="i-header">定&nbsp;&nbsp;&nbsp;&nbsp;价</span>
+                        </el-col>
+                        <el-col :span="8">
+                            <span class="mainPrice">人民币: ￥{{formatPrice(goodsInfo.specNowPrice)}}</span>
+                        </el-col>
+                        <el-col :span="4">
+                            <span class="minorPrice">泰拉 </span><strong class="minorPrice">{{formatPrice(goodsInfo.specNowPrice/nalaRate)}}</strong>
+                        </el-col>
+                        <el-col :span="4">
+                            <span class="minorPrice">美元 $</span><strong class="minorPrice">{{formatPrice(goodsInfo.specNowPrice/dollarRate)}}
+                        </strong></el-col>
+                    </el-row>
+                </div>
+                <div class="proinfo-num">
+                    <el-row style="margin-top: 10px; vertical-align: middle;">
+                        <el-col :span="2">
+                            <span class="i-header">数&nbsp;&nbsp;&nbsp;&nbsp;量</span>
+                        </el-col>
+                        <el-col :span="12">
+                            <div class="num-">&nbsp;-&nbsp;</div>&nbsp;
+                            <div class="numBuy">{{numBuy}}</div>
+                            <div class="numplus">&nbsp;+&nbsp;</div>&nbsp;
+                        </el-col>
+                    </el-row>
+                </div>
             </div>
+
         </div>
     </div>
 </template>
@@ -24,6 +72,7 @@
     import {getGoodsserialById} from "../../../util/module";
     import * as cfg from "../../../config/cfg";
     import {trimSpace} from "../../../Gw/GwArray";
+    import _String from '@/util/string';
 
     export default {
         name: "goodsSale",
@@ -38,13 +87,17 @@
                 pictureUrl:'',
                 imgIndex:0,
                 timer:[],
+                dollarRate:1,
+                nalaRate:1,
+                numBuy:0,
             }
         },
 
         created(){
             this.specGoodsId=this.$route.query.specGoodsId;
             this.pictureUrl = cfg.service.uploadUrl + '/';
-            console.log('specGoodsId', this.specGoodsId);//debug
+            this.nalaRate=localStorage.getItem('nalaRate')||1;
+            this.dollarRate=localStorage.getItem('dollarRate')||1;
             this.getGoodsInfo();
         },
 
@@ -67,6 +120,11 @@
                 ).catch();
             },
 
+            //格式化金额
+            formatPrice(price) {
+                return _String.number_format(price, 2);
+            },
+
             mouseenter(item, index){
                 this.timer[index]=setTimeout(()=>{
                     this.imgShow=item;
@@ -82,8 +140,12 @@
 </script>
 
 <style scoped>
+    .mainDiv{
+        position: relative;
+    }
+
     .proinfoLeft{
-        border: 1px solid red;
+        position: absolute;
     }
 
     .imgzoom-main{
@@ -98,8 +160,8 @@
     }
 
     .imgzoom-thumb{
-        width: 400px;
-        margin: 15px 32px 0 32px;
+        width: 350px;
+        margin: 15px 20px 0 20px;
         overflow: hidden;
     }
 
@@ -117,6 +179,98 @@
     .imgzoom-thumb li img{
         height: 60px;
         width: 60px;
+    }
+
+
+    .proinfoMain{
+        height: 692px;
+        margin-left: 400px;
+        padding-left: 10px;
+        font: normal 12px/1.5 Arial,Microsoft YaHei,SimSun;
+        color: #666;
+    }
+
+    .proinfo-title{
+        padding: 13px 10px 12px;
+        background: #fff;
+    }
+
+    .proinfo-title h1{
+        font: 700 16px/1.5 \5FAE\8F6F\96C5\9ED1;
+        color: #222;
+        min-height: 24px;
+    }
+
+    .goods-publish{
+        border-top: 1px dotted #ddd;
+        padding: 12px 0 0;
+        margin-bottom: 16px;
+    }
+
+    .i-header{
+        color: #999;
+        margin-left: 10px;
+        height: 32px;
+        line-height: 32px;
+    }
+
+    .proinfo-focus{
+        padding: 14px 0 8px;
+        margin-bottom: 15px;
+        background-color: rgb(245,245,245);
+    }
+
+    .mainPrice{
+        height: 28px;
+        float: left;
+        font: 700 30px/25px Tahoma;
+        color: #e00;
+    }
+
+    .minorPrice{
+        height: 28px;
+        float: left;
+        font: 400 20px/25px Tahoma;
+    }
+
+    .proinfo-num{
+        border-top: 1px dotted #ddd;
+        vertical-align: middle;
+    }
+
+    .num-{
+        display: inline-block;
+        border: 1px solid rgb(221,221,221);
+        height: 32px;
+        width: 32px;
+        vertical-align: middle;
+        line-height: 32px;
+        text-align: center;
+        background-color: rgb(241,241,241);
+        font-size: 20px;
+    }
+
+    .numplus{
+        display: inline-block;
+        border: 1px solid rgb(221,221,221);
+        height: 32px;
+        width: 32px;
+        vertical-align: middle;
+        line-height: 32px;
+        text-align: center;
+        background-color: rgb(241,241,241);
+        margin-left: 8px;
+        font-size: 20px;
+    }
+
+    .numBuy{
+        display: inline-block;
+        border: 1px solid rgb(221,221,221);
+        height: 32px;
+        width: 64px;
+        vertical-align: middle;
+        line-height: 32px;
+        text-align: center;
     }
 
     .redBorder{
